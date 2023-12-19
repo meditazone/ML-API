b6121123db0ff79641f4cf50cb05a25c4a4fb09e
Riferdinand <113415789+Riferdinand@users.noreply.github.com>
2023-12-19 21:45:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,79 +1,63 @@
 from flask import Flask, request, jsonify
-from tensorflow.keras.models import load_model
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from flask_cors import CORS
+from tensorflow import keras
+from keras.models import load_model
+from keras.preprocessing.sequence import pad_sequences
 import numpy as np
 import pickle
-import logging
-import os
 
 app = Flask(__name__)
-CORS(app)
 
-# Load model di awal aplikasi
-app.config['MODEL_PATH'] = os.environ.get('MODEL_PATH', 'model.h5')
-model = load_model(app.config['MODEL_PATH'])
+labels = ["anxiety", "depression", "stress"]
 
-# Tambahkan layer softmax
-num_classes = 3  # Sesuaikan dengan jumlah kelas Anda
-model.add(Dense(num_classes, activation='softmax', name='output'))
+# Load the tokenizer and model from local storage
+with open('tokenizer.pkl', 'rb') as handle:
+    tokenizer = pickle.load(handle)
 
-# Load the saved Tokenizer
-app.config['TOKENIZER_PATH'] = os.environ.get('TOKENIZER_PATH', 'tokenizer.pkl')
-with open(app.config['TOKENIZER_PATH'], 'rb') as tokenizer_file:
-    tokenizer = pickle.load(tokenizer_file)
+model = keras.models.load_model('model.h5')
 
-# Setup logging
-logging.basicConfig(level=logging.INFO)
+def prepare_input(text):
+    sequences = tokenizer.texts_to_sequences([text])
+    padded = pad_sequences(sequences, maxlen=250, truncating='post', padding='post')
+
+    return padded
+
+def get_prediction(text):
+    # Prepare the input text
+    input_text = prepare_input(text)
+
+    # Use the model to perform predictions
+    predictions = model.predict(input_text)
+
+    # Find the index of the class with the highest probability
+    prediction_index = np.argmax(predictions, axis=1)[0]
+
+     # Create the result dictionary
+    result = {
+        'text': text,
+        'predictions': [
+            {'class': labels[j], 'probability': float(predictions[0][j]) * 100}  # Multiply by 100 for percentage
+            for j in range(len(predictions[0]))
+        ],
+        'predicted_class': labels[prediction_index],
+        'predicted_probability': float(predictions[0][prediction_index] * 100)
+    }
+    
+    return result
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    try:
-        # Validasi input
-        data = request.get_json(force=True)
-        if 'text' not in data or not isinstance(data['text'], str):
-            return jsonify({'error': "'text' key not found or not a valid string in data"})
-        user_text = data['text']
+    # Get the input text from the request data
+    data = request.get_json()
+    text = data['text']
 
-        # Load the saved Tokenizer
-        with open(app.config['TOKENIZER_PATH'], 'rb') as tokenizer_file:
-            tokenizer = pickle.load(tokenizer_file)
+    # Perform predictions using the input text
+    prediction = get_prediction(text)
 
-        # Pre-process the input data
-        sequences = tokenizer.texts_to_sequences([user_text])
-        padded_data = pad_sequences(sequences, maxlen=250, padding='post', truncating='post')
-
-        # Make predictions using the loaded model
-        predictions = model.predict(padded_data)
-
-        # Define labels here
-        labels = ["Anxiety", "Depression", "Stress"]
-
-        # Process predictions and return the results
-        results = []
-        result = {
-            'text': user_text,
-            'predictions': [
-                {
-                    'class': labels[j], 'probability': float(predictions[0][j])
-                }
-                for j in range(len(predictions[0]))
-            ],
-            'predicted_class': labels[np.argmax(predictions[0])]
-        }
-        results.append(result)
-
-        # Log request and response
-        logging.info('Received request: %s', data)
-        logging.info('Sending response: %s', jsonify(results).json)
-
-        return jsonify(results)
-
-    except Exception as e:
-        logging.error('Error: %s', str(e))
-        return jsonify({'error': str(e)})
-
+    # Return the prediction as a JSON response
+    response = {
+        'prediction': prediction
+    }
+    return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
+    app.run(debug=True)
